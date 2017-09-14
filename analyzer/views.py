c2b9371ec613d9dffa9572aaf1fde823a0fe1014
jcolombo24 <j.colombo01@gmail.com>
2017-09-13 18:29:39 -0700

    Update views.py

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.urls import reverse
 from .models import pitcher
 from django_datatables_view.base_datatable_view import BaseDatatableView
-from django.template import RequestContext
 import json
 import operator
-from django.http.response import HttpResponse
-    
+
+def pitcher_view(request):
+    return render(
+        request,
+        'analyzer/index.html',
+    )
+          
 class pitcherlist(BaseDatatableView):
     model = pitcher
     columns = ['player_name', 'game_date','pitch_type', 'pfx_x']
-    order_columns = ['player_name', 'game_date','pitch_type', 'pfx_x']
-    max_display_length = 500
     
     def render_column(self, row, column):
         return super(pitcherlist, self).render_column(row, column)
     
-    def get_initial_queryset(self):
-        return pitcher.objects.all()
+    def prepare_results(self):
+        json_data = [pitcher.objects.all()]
+        return json_data
+    
     
