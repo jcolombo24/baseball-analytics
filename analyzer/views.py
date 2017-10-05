a6b21e494ef72f996f7b7e05fcfeb6728539afdb
jcolombo24 <j.colombo01@gmail.com>
2017-10-04 23:20:32 -0700

    Update views.py

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,26 +1,37 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.urls import reverse
+from django.http import HttpResponse
 from .models import pitcher
+from django.urls import reverse
 from django_datatables_view.base_datatable_view import BaseDatatableView
 import json
-import operator
+from analyzer.forms import PitcherFilterForm
 
 def pitcher_view(request):
+    form = PitcherFilterForm(request.POST)
+    data = reverse('pitcherlist')
+    if request.method == 'POST' and request.is_ajax:
+        params = request.POST.copy()
+        data = ('%s?%s' % (reverse('pitcherlist'), ''.join(
+            ['%s=%s&' % (k, v) for k, v in params.items()])))\
+            .rstrip('&')
+
+        return HttpResponse(json.dumps(data), content_type='application/json')
+    
     return render(
         request,
         'analyzer/index.html',
+        {'form': form}
     )
           
-class pitcherlist(BaseDatatableView):
+class pitcher_list_json(BaseDatatableView):
     model = pitcher
-    columns = ['player_name', 'game_date','pitch_type', 'pfx_x']
+    columns = ['player_name', 'game_date','pitch_type', 'release_speed', 'pfx_x']
+    order_columns = ['player_name', 'game_date','pitch_type', 'release_speed', 'pfx_x']
     
     def render_column(self, row, column):
-        return super(pitcherlist, self).render_column(row, column)
+            return super(pitcher_list_json, self).render_column(row, column)
     
-    def prepare_results(self):
-        json_data = [pitcher.objects.all()]
-        return json_data
-    
-    
+    def filter_queryset(self, qs):
+        search = pitcher.objects.all()
+        return qs
