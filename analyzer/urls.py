6fbd0418252f1381b1031d6c04cbbb73524f6423
jcolombo24 <j.colombo01@gmail.com>
2017-10-04 23:20:57 -0700

    Update urls.py

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import url
 from analyzer import views
 from django.conf.urls.static import static, settings
-from analyzer.views import pitcherlist, pitcher_view
-from django.urls import reverse
-import json
-
+from analyzer.views import pitcher_list_json, pitcher_view
 
 urlpatterns = [
-    url(r'^$', views.pitcher_view, name='pitcherview'),
-    url(r'^$', pitcherlist.as_view(), name='pitcherlist'),
+    url(r'^$', views.pitcher_view),
+    url(r'^analyzer/$', pitcher_list_json.as_view(), name='pitcherlist'),
+    
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
