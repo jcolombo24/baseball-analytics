26d8a0d553c4a8cc5e801c958620bb4647998557
jcolombo24 <j.colombo01@gmail.com>
2017-10-04 23:19:17 -0700

    Update forms.py

--- analyzer/forms.py
+++ analyzer/forms.py
@@ -1,11 +1,29 @@
+from datetime import date, timedelta
+import itertools
 from django import forms
+import operator
 from .models import pitcher
+from crispy_forms.helper import FormHelper
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.layout import Layout, Button, Submit, MultiField, Div, HTML, \
+    Field, Fieldset, Reset
 
-class pitcherform(forms.ModelForm):
-    class Meta:
-        model = pitcher
-        fields = ('player_name')
+DATE_FORMAT = '%m/%d/%Y'
 
-    def __init__(self, user, *args, **kwargs):
-        super(pitcherform, self).__init__(*args, **kwargs)
-        self.fields['player_name'].queryset = Category.objects.all
+class PitcherFilterForm(forms.Form):
+    player_name = forms.ModelChoiceField(
+        queryset=pitcher.objects.order_by('player_name').values_list('player_name', flat=True).distinct(), required=True)
+    
+    def __init__(self, *args, **kwargs):
+        super(PitcherFilterForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'filter_form'
+        self.helper.form_method = 'post'
+        self.helper.form_action = '.'
+        self.helper.layout = Layout(
+            Field('player_name'),
+            FormActions(
+                Submit('submit', 'Filter'),
+                Reset('reset', 'Reset'),),
+                )
+                           
